7b61c6a08b029066e30f1e7cbf5898fac9fd79a6
Weird0 <emtp.price1243@gmail.com>
2024-11-30 20:17:47 -0600

    Setting up logger + more cleanup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Some bot
-I'm making a discord bot to both relearn API magic and to learn the git platform, I am simply messing around with this creation. 
+I'm making a discord bot to both relearn API magic and to learn the git platform, I am simply messing around with this creation. I might make something serious someday. 
+
+> "Innovation? nah, it's one big joke!"
+
+Kinda, kinda not, we'll see!
--- bot/newbot.py
+++ bot/newbot.py
@@ -5,21 +5,31 @@ description="""A simple management bot to fit all your needs.
 for any quetions reach out to me @weird0cats"""
 intents=nextcord.Intents.default()
 intents.members=True
-intents.message_content
+intents.message_content=True
 
 bot=commands.Bot(command_prefix="|", description=description, intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}, ID:{bot.user.id}")
+    print(f"""Logged in as {bot.user}, ID:{bot.user.id}
+Wrapper version:{nextcord.__version__}""")
 
+@bot.command()
+async def color(ctx):
+    embed=nextcord.Embed(title="test")
+    await ctx.send("what")
+
+#slash commands, to remain seperate from other commands, as they're simpler to use
 @bot.slash_command(description="Test the bot")
 async def test(interaction:nextcord.Interaction):
     await interaction.send("test successful", ephemeral=True)
 
 @bot.slash_command(description='Get a list of commands')
 async def help(interaction=nextcord.Interaction):
-    embed="embed"
-    interaction.send(embed)
+    embed=nextcord.Embed(colour=0xdc143c,title='commands',description=f"""
+    {bot.command_prefix}color - prints a list of colors
+    
+    """)
+    await interaction.send(embed=embed,ephemeral=True)
 
 bot.run(config.tok)

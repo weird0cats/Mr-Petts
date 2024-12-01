05faeb0c8137db3d686c5618ecaff55904f5446b
Weird0 <emtp.price1243@gmail.com>
2024-12-01 10:15:54 -0600

    Changing the idea, pet simulator time!

--- bot/newbot.py
+++ bot/newbot.py
@@ -1,5 +1,7 @@
-import logging, nextcord, os, config
-from nextcord import Interaction
+
+
+import logging, nextcord, os, config, jsonfs
+from nextcord import Interaction, SlashOption
 from nextcord.ext import commands, application_checks
 
 #Logs 
@@ -31,7 +33,7 @@ intents.members=True
 intents.message_content=True
 
 bot=commands.Bot(
-    command_prefix="",
+    command_prefix="$",
     description=description,
     intents=intents
     )
@@ -39,14 +41,12 @@ bot=commands.Bot(
 
 @bot.event
 async def on_ready():
-    logging.log(msg=f"""Logged in as {bot.user}, ID:{bot.user.id}
-Powered by Nextcord {nextcord.__version__}""",level=logging.INFO)
+    print(f"""Logged in as {bot.user}, ID:{bot.user.id}
+Powered by Nextcord {nextcord.__version__}""")
 
 @bot.command()
 async def test(ctx):
-    embed=nextcord.Embed(title="test")
-    await ctx.send("test")
-    await ctx.send(embed=embed)
+    await ctx.send("It works!")
 
 #slash commands, to remain seperate from other commands, as they're simpler to use and I refuse to keep track of them. 
 @bot.slash_command(description="Test the bot")
@@ -54,11 +54,12 @@ async def test(interaction:Interaction):
     await interaction.send("test successful", ephemeral=True)
 
 @bot.slash_command(description='Get a list of commands')
-async def help(interaction=Interaction):
+async def helpme(interaction:Interaction):
     embed=nextcord.Embed(colour=0xdc143c,title='commands',description=f"""
     {bot.command_prefix}c - "colors" - prints a list of colors
     {bot.command_prefix}m -  - 
     """)
     await interaction.send(embed=embed,ephemeral=True)
+#until I can fix standard commands
 
 bot.run(config.tok)
